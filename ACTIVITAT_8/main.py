53284a95cf008e476366c7ac5c49012791964298
davidsc2024 <2024_davids.carbajal@iticbcn.cat>
2024-12-07 21:54:32 +0100

    ejercicio 5 basemodel - añadir mas campos

--- ACTIVITAT_8/main.py
+++ ACTIVITAT_8/main.py
@@ -3,30 +3,28 @@ from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-# Inicialización de la app
 app = FastAPI()
 
 
-# Modelo de datos para manejar entradas (POST)
 class Item(BaseModel):
     name: str
     price: float
     is_offer: Union[bool, None] = None
+    description: Union[str, None] = None  # Campo opcional
+    stock: int  # Cantidad disponible
+    category: str  # Categoría del producto
 
 
-# Método GET en la raíz
 @app.get("/")
 def read_root():
-    return {"message": "Bienvenido a mi API"}
+    return {"Hello": "World"}
 
 
-# Método GET para consultar un ítem
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 
-# Método POST para crear un nuevo ítem
 @app.post("/items/")
 def create_item(item: Item):
-    return {"message": "Item creado exitosamente", "item": item}
+    return {"name": item.name, "price": item.price, "category": item.category}
