b3ddf15952e61e76d680261fc37f6ff5bf7bd0c3
davidsc2024 <2024_davids.carbajal@iticbcn.cat>
2024-12-07 21:40:30 +0100

    metodos get y post

--- ACTIVITAT_8/main.py
+++ ACTIVITAT_8/main.py
@@ -3,25 +3,30 @@ from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+# Inicialización de la app
 app = FastAPI()
 
 
+# Modelo de datos para manejar entradas (POST)
 class Item(BaseModel):
     name: str
     price: float
     is_offer: Union[bool, None] = None
 
 
+# Método GET en la raíz
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"message": "Bienvenido a mi API"}
 
 
+# Método GET para consultar un ítem
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_price": item.price, "item_id": item_id}
+# Método POST para crear un nuevo ítem
+@app.post("/items/")
+def create_item(item: Item):
+    return {"message": "Item creado exitosamente", "item": item}
