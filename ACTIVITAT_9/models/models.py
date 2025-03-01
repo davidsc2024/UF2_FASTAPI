fd7332cacf3c60115cf9277e70ebdd34efad038d
davidsc2024 <2024_davids.carbajal@iticbcn.cat>
2025-03-01 19:12:30 +0100

    modifications in database config

---
+++ ACTIVITAT_9/models/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"  # Nombre de la tabla en la BD
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
